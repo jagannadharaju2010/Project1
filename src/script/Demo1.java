87ab4571286eed3d9fcb28d37a57914449f5aa90
bob <bob@10.0.2.93>
2018-11-26 10:15:17 +0530

    Update

--- src/script/Demo1.java
+++ src/script/Demo1.java
@@ -10,13 +10,14 @@ import generic.Utility;
 import page.DashboardPage;
 import page.DirectoryPage;
 import page.LoginPage;
+import page.LoginPageP2P;
 
 public class Demo1 extends BaseTest {
 	@Test
 	public void testA(){
 		String un = Utility.getXLData(DATA_PATH, "Sheet1", 1, 1);
 		String pw=Utility.getXLData(DATA_PATH, "Sheet1", 1, 2);
-		LoginPage l=new LoginPage(driver);
+		/*LoginPage l=new LoginPage(driver);
 		l.setUserName(un);
         l.setPassword(pw);
         l.clickLogin();
@@ -24,7 +25,11 @@ public class Demo1 extends BaseTest {
         dp.clickbtn();
         DashboardPage d=new DashboardPage(driver);
         d.clickWelcome();
-        d.clickLogout(driver, 10);
+        d.clickLogout(driver, 10);*/
+		LoginPageP2P l=new LoginPageP2P(driver);
+		l.username(un);
+		l.password(pw);
+		l.signButton();
 		
 	}
 }

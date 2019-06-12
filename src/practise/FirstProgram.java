7d574d22de288cc9b0ea5c176829c795cf736a12
bob <bob@10.0.2.93>
2019-06-12 14:17:42 +0530

    Commit to repository

---
+++ src/practise/FirstProgram.java
@@ -0,0 +1,29 @@
+package practise;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstProgram {
+	
+	static{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	
+	public static void meth() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.close();
+	}
+	
+	public static void meth2() {
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.close();
+	}
+
+	public static void main(String[] args) {
+      FirstProgram.meth();
+	}
+
+}
